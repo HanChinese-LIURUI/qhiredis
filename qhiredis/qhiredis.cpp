6418a619649e1091fc666711209784a34c815e44
刘锐 <1435452662@qq.com>
2024-08-31 19:16:03 +0800

    添加项目文件。

---
+++ qhiredis/qhiredis.cpp
@@ -0,0 +1,318 @@
+﻿#include "qhiredis.h"
+
+QHiRedis::QHiRedis(const QString& ip, const qint64& port, QObject* parent)
+	:QObject(parent),
+	rIp(ip),
+	rPort(port),
+	redis(nullptr) {
+
+}
+
+QHiRedis::~QHiRedis() {
+}
+
+bool QHiRedis::connectServer(
+	QString* const error,
+	const QString& useName,
+	const QString& passWord,
+	const int& timeOut) {
+
+	bool state = true;
+	timeval time{ 0,timeOut };
+
+	redis = redisConnectWithTimeout(rIp.toStdString().c_str(), rPort, time);
+	if (!redis) {
+		state = false;
+	}
+	else {
+		*error = QString(redis->errstr);
+		if (redis->err != 0) {
+			state = false;
+		}
+	}
+	//验证密码
+	if (state && !useName.isEmpty() && !passWord.isEmpty()) {
+		redisReply* reply = (redisReply*)redisCommand(
+			redis,
+			QString("AUTH %1").arg(useName).toStdString().c_str(),
+			passWord.toStdString().c_str());
+
+		if (reply->type == REDIS_REPLY_ERROR) {
+			state = false;
+		}
+		else {
+			*error = "Authentication failure";
+		}
+		freeReplyObject(reply);
+	}
+	return state;
+}
+
+bool QHiRedis::ping() const {
+	bool state = true;
+	redisReply* reply = (redisReply*)redisCommand(redis, "PING");
+
+	if (reply) {
+		if (reply->type == REDIS_REPLY_ERROR) {
+			state = false;
+		}
+		else if (QString(reply->str) != "PONG") {
+			state = false;
+		}
+	}
+	else {
+		state = false;
+	}
+
+	return state;
+}
+
+bool QHiRedis::del(const QVariant& key) {
+	bool state = true;
+	QString command = "DEL " + key.toString();
+
+	redisReply* reply = (redisReply*)redisCommand(redis, command.toStdString().c_str());
+
+	if (reply) {
+		if (reply->type != REDIS_REPLY_INTEGER) {
+			state = false;
+		}
+		//else {
+		//	reply->integer != 1 ? state = false : state = true;
+		//}
+	}
+	else {
+		state = false;
+	}
+
+	return state;
+}
+
+bool QHiRedis::exists(const QVariant& key) {
+	bool state = true;
+	QString command = "EXISTS " + key.toString();
+
+	redisReply* reply = (redisReply*)redisCommand(redis, command.toStdString().c_str());
+
+	if (reply) {
+		if (reply->type != REDIS_REPLY_INTEGER) {
+			state = false;
+		}
+		else {
+			reply->integer != 1 ? state = false : state = true;
+		}
+	}
+	else {
+		state = false;
+	}
+
+	return state;
+}
+
+bool QHiRedis::setString(const QVariant& key, const QVariant& value) const {
+	bool state = true;
+	QString command = "SET " + key.toString() + " " + value.toString();
+	redisReply* reply = (redisReply*)redisCommand(redis, command.toStdString().c_str());
+
+	if (reply) {
+		if (reply->type == REDIS_REPLY_ERROR) {
+			state = false;
+		}
+		else if (QString(reply->str) != "OK") {
+			state = false;
+		}
+	}
+	else {
+		state = false;
+	}
+
+	return state;
+}
+
+bool QHiRedis::setnxString(const QVariant& key, const QVariant& value) const {
+	bool state = true;
+	QString command = "SETNX " + key.toString() + " " + value.toString();
+	redisReply* reply = (redisReply*)redisCommand(redis, command.toStdString().c_str());
+
+	if (reply) {
+		if (reply->type != REDIS_REPLY_INTEGER) {
+			state = false;
+		}
+		else if (reply->integer != 1) {
+			state = false;
+		}
+	}
+	else {
+		state = false;
+	}
+
+	return state;
+}
+
+bool QHiRedis::mSetString(const QVariantMap& map) const {
+	bool state = true;
+	QString command = "MSET ";
+	for (QString var : map.keys()) {
+		command += var + " " + map.value(var).toString() + " ";
+
+	}
+
+	redisReply* reply = (redisReply*)redisCommand(redis, command.toStdString().c_str());
+
+	if (reply) {
+		if (reply->type == REDIS_REPLY_ERROR) {
+			state = false;
+		}
+		else if (QString(reply->str) != "OK") {
+			state = false;
+		}
+	}
+	else {
+		state = false;
+	}
+
+	return state;
+}
+
+bool QHiRedis::mSetnxString(const QVariantMap& map) const {
+	bool state = true;
+	QString command = "MSETNX ";
+	for (QString var : map.keys()) {
+		command += var + " " + map.value(var).toString() + " ";
+
+	}
+
+	redisReply* reply = (redisReply*)redisCommand(redis, command.toStdString().c_str());
+
+	if (reply) {
+		if (reply->type != REDIS_REPLY_INTEGER) {
+			state = false;
+		}
+		else {
+			if (reply->integer != 1) {
+				state = false;
+			}
+		}
+	}
+	else {
+		state = false;
+	}
+
+	return state;
+}
+
+QVariant QHiRedis::getString(const QVariant& key) const {
+	QVariant value;
+
+	QString command = "GET " + key.toString();
+	redisReply* reply = (redisReply*)redisCommand(redis, command.toStdString().c_str());
+
+	if (reply) {
+		if (reply->type == REDIS_REPLY_STRING) {
+			value = QString(reply->str);
+		}
+	}
+
+	return value;
+}
+
+bool QHiRedis::incrString(const QVariant& key, qint64* const value) const {
+	bool state = true;
+	*value = 0;
+
+	QString command = "INCR " + key.toString();
+	redisReply* reply = (redisReply*)redisCommand(redis, command.toStdString().c_str());
+
+	if (reply) {
+		if (reply->type == REDIS_REPLY_INTEGER) {
+			*value = reply->integer;
+		}
+		else {
+			state = false;
+		}
+	}
+	else {
+		state = false;
+	}
+
+	return state;
+}
+
+bool QHiRedis::decrString(const QVariant& key, qint64* const value) const {
+	bool state = true;
+	*value = 0;
+
+	QString command = "DECR " + key.toString();
+	redisReply* reply = (redisReply*)redisCommand(redis, command.toStdString().c_str());
+
+	if (reply) {
+		if (reply->type == REDIS_REPLY_INTEGER) {
+			*value = reply->integer;
+		}
+		else {
+			state = false;
+		}
+	}
+	else {
+		state = false;
+	}
+
+	return state;
+}
+
+bool QHiRedis::setHash(const QVariant& key, const QVariantHash& value) const {
+	bool state = true;
+
+	QString command = "HMSET " + key.toString() + " ";
+	for (QString var : value.keys()) {
+		command += var + " " + value[var].toString() + " ";
+	}
+
+	redisReply* reply = (redisReply*)redisCommand(redis, command.toStdString().c_str());
+
+	if (reply) {
+		if (reply->type == REDIS_REPLY_ERROR) {
+			state = false;
+		}
+	}
+	else {
+		state = false;
+	}
+
+	return state;
+}
+
+QVariantHash QHiRedis::getHash(const QVariant& key) const {
+	QVariantHash valueHash;
+
+	QString command = "HGETALL " + key.toString();
+	redisReply* reply = (redisReply*)redisCommand(redis, command.toStdString().c_str());
+
+	if (reply) {
+		if (reply->type == REDIS_REPLY_ARRAY) {
+			size_t size = reply->elements;
+			QString key, value;
+			for (size_t i = 0; i < size; i++) {
+
+				redisReply* tReply = reply->element[i];
+
+				if (tReply) {
+
+					if (tReply->type == REDIS_REPLY_STRING) {
+						QString tString = tReply->str;
+						if (i % 2 == 0)
+							key = tString;
+						else {
+							value = tString;
+							valueHash.insert(key, value);
+							key.clear();
+							value.clear();
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return valueHash;
+}
---
+++ qhiredis/qhiredis.h
@@ -0,0 +1,112 @@
+﻿#ifndef QHIREDIS_H__
+#define QHIREDIS_H__
+
+#if _MSC_VER
+#pragma warning(disable : 4200)
+
+struct timeval {
+	long tv_sec;     // 秒
+	long tv_usec;    // 微秒
+};
+
+#endif
+#if _MSC_VER >= 1600
+#pragma execution_character_set("utf-8")
+#endif
+
+#include "qhiredis_global.h"
+#include "hiredis.h"
+#include <QObject>
+#include <QVariant>
+#include <QHash>
+
+class QHiRedis : public QObject {
+	Q_OBJECT
+
+public:
+	explicit QHiRedis(const QString& ip = "127.0.0.1", const qint64& port = 6379, QObject* parent = nullptr);
+	~QHiRedis();
+
+	//! @brief 连接服务器
+	//! @param timeOut 阻塞连接
+	//! @param error 返回的错误状态
+	//! @return 成功:true 
+	bool connectServer(
+		QString* const error,
+		const QString& useName = "",
+		const QString& passWord = "",
+		const int& timeOut = 1000);
+
+	//! @brief 检测服务是否正常
+	//! @return 
+	bool ping() const;
+
+	//! @brief 删除指定的key
+	//! @param key 
+	//! @return 成功:true(不存在这个数据也会返回true)
+	bool del(const QVariant& key);
+
+	//! @brief 检查给定key是否存在
+	//! @param key 
+	//! @return 存在:true
+	bool exists(const QVariant& key);
+
+	//! @brief 设置一个key-value对，key如果存在，则更新value。
+	//! @param key 
+	//! @param value 
+	//! @return 成功:true 
+	bool setString(const QVariant& key, const QVariant& value) const;
+
+	//! @brief 只有在key不存在时设置key的值
+	//! @param key 
+	//! @param value 
+	//! @return 成功:true(代表数据不存在且插入成功) false:(数据插入失败，但不代表一定是数据存在返回的失败)
+	bool setnxString(const QVariant& key, const QVariant& value) const;
+
+	//! @brief 同时设置一个或多个key-value对，key如果存在，则更新value。
+	//! @param map 
+	//! @return 
+	bool mSetString(const QVariantMap& map) const;
+
+	//! @brief 同时设置一个或多个key-value对，当且仅当所有给定key都不存在
+	//! @param key 
+	//! @param value 
+	//! @return 成功:true(代表所有的数据都不存在且插入成功) false:(数据插入失败，但不代表一定是数据存在返回的失败)
+	bool mSetnxString(const QVariantMap& map) const;
+
+	//! @brief 获取一个key字符串值
+	//! @param key 
+	//! @return 
+	QVariant getString(const QVariant& key) const;
+
+	//! @brief 将key的值自增1(value必须为数字值),如果不存在key则先插入再自增
+	//! @param key 
+	//! @param value 
+	//! @return 成功:true 
+	bool incrString(const QVariant& key, qint64* const value) const;
+
+	//! @brief 将key的值自减1(value必须为数字值),如果不存在key则先插入再自减
+	//! @param key 
+	//! @param value 
+	//! @return 
+	bool decrString(const QVariant& key, qint64* const value) const;
+
+	//! @brief 添加一个Hash
+	//! @param key 
+	//! @param value 
+	//! @return 成功:true 
+	bool setHash(const QVariant& key, const QVariantHash& value) const;
+
+	//! @brief 获取一个keyHash值
+	//! @param key 
+	//! @return 
+	QVariantHash getHash(const QVariant& key) const;
+protected:
+
+private:
+	QString rIp;
+	qint64 rPort;
+	redisContext* redis;
+};
+
+#endif // QHIREDIS_H__
